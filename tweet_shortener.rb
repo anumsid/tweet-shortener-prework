b0e623218017bc7fdf13a729066c51e0c633e90e
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-04-11 21:19:49 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def word_substituter(tweet)
+  dictionary = {
+    "hello": "hi",
+    "to": "2",
+    "two": "2",
+    "too": "2",
+    "for": "4",
+    "four": "4",
+    "be": "b",
+    "you": "u",
+    "at": "@",
+    "and": "&"
+  }
+
+  tweetarr = tweet.split(" ").collect do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
+    end  
+  end
+  tweetarr.join(" ")  
+end
